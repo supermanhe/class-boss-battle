2745e14386bd36098121f9cdcc273872e67d4c7b
supermanhe <supermanhe@example.com>
2025-04-28 18:50:33 +0800

    fix: Express路由错误，将通配符 * 改为 /* 解决部署问题

--- server/server.js
+++ server/server.js
@@ -66,7 +66,7 @@ app.delete('/scores', (req, res) => {
 // 静态托管前端 build 目录
 app.use(express.static(path.join(__dirname, '../build')));
 // SPA 兜底路由，所有未知路由返回 index.html
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../build/index.html'));
 });
 
